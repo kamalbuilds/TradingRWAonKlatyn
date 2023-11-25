395de709afceb91a67ecbc7dcf8bf4d134015955
Kamal Nayan <geniusamansingh@gmail.com>
2023-11-25 18:37:45 +0530

    build fix

--- const/contractAddresses.ts
+++ const/contractAddresses.ts
@@ -7,7 +7,7 @@ export const MARKETPLACE_ADDRESS = "0x7708b64D9235603995C85702877371a796919DfF";
 // 3. The address of your NFT collection smart contract.
 export const NFT_COLLECTION_ADDRESS ="0x0A8F2D4777D8228cB1893ad6582D659102afAADa";
 
-export const RENTNFT_COLLECTION_ADDRESS ="0x7e17031c3A75c670fb5dC22048437ED91d738927";
+export const RENTNFT_COLLECTION_ADDRESS ="";
 
 // (Optional) Set up the URL of where users can view transactions on
 export const ETHERSCAN_URL = "https://baobab.klaytnscope.com/";
