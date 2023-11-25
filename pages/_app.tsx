55e0f7e4c980d3803ad65abd604d5c1b3fa85f5b
Kamal Nayan <geniusamansingh@gmail.com>
2023-11-25 18:38:49 +0530

    build fix 3

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,33 @@ import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThirdwebProvider activeChain={NETWORK} >
+    <ThirdwebProvider
+    // Set active chain for app
+    activeChain={{
+
+      chainId: 1001, // Chain ID of the network
+      // Array of RPC URLs to use
+      rpc: ["https://public-en-baobab.klaytn.net"],
+
+      // === Information for adding the network to your wallet (how it will appear for first time users) === \\
+      // Information about the chain's native currency (i.e. the currency that is used to pay for gas)
+      nativeCurrency: {
+        decimals: 18,
+        name: "Klaytn Testnet",
+        symbol: "KLAY",
+      },
+      shortName: "Klaytn Testnet", // Display value shown in the wallet UI
+      slug: "klatyntestnet", // Display value shown in the wallet UI
+      testnet: true, // Boolean indicating whether the chain is a testnet or mainnet
+      chain: "Klaytn Testnet", // Name of the network
+      name: "Klaytn Testnet", // Name of the network
+    }}
+    // Auth (SIWE) configuration
+    authConfig={{
+      domain: process.env.NEXT_PUBLIC_AUTH_DOMAIN || "evmkit.com", // Your website domain
+      authUrl: "/api/auth", // API Route (default is - pages/api/auth/[...thirdweb].ts)
+    }}
+  >
       {/* Progress bar when navigating between pages */}
       <NextNProgress
         color="var(--color-tertiary)"
